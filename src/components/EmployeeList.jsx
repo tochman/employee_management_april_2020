38456bccd352c51cd665a5b31bb4304bd2120149
Thomas <thomas@craftacademy.se>
2020-04-27 17:18:31 +0200

    adds component tests for EmployeeList Adds EmployeeList component that fetches data from API

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class EmployeeList extends Component {
+  state = {
+    employees: []
+  }
+
+  componentDidMount() {
+    axios.get('https://reqres.in/api/users?per_page=4')
+      .then(response => {
+        // do something with the response
+        this.setState({ employees: response.data.data })
+      })
+  }
   render() {
     return (
       <ul>
-
+        {
+          this.state.employees.map(employee => {
+            return (
+              <li key={employee.id}>{`${employee.first_name} ${employee.last_name}`}</li>
+            )
+          })
+        }
       </ul>
     );
   }
