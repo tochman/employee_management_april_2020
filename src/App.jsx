cac64c60a997f629a3624281af148fac3bd18eb1
Thomas <thomas@craftacademy.se>
2020-04-27 13:21:19 +0200

    cleans up scaffolded code

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <section name="header">
+          <h1>Employee list</h1>
+        </section>
+
+        <section name="main">
+
+        </section>
+      </>
+    );
+  }
+}
+
+export default App;
